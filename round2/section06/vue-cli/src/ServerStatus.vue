658ec30ab2d581aa9f16d94fd9764aef99197fa4
Katy <ihatetoast@gmail.com>
2017-07-09 23:03:07 -0500

    switching from simple vue instances to local and global components

---
+++ round2/section06/vue-cli/src/ServerStatus.vue
@@ -0,0 +1,22 @@
+<template>
+	<div>
+		<p>Server status: {{ status }}</p>
+		<hr/>
+		<button @click="changeStatus">Change</button>
+	</div>
+</template>
+<script>
+	export default{
+		data: function(){
+			return {
+				status: 'Critical we are all doing to die!!'
+			}
+		}, 
+		methods: {
+			//can make it ES6y
+			changeStatus(){
+				this.status = "All good. We can assume there will be a tomorrow.";
+			}
+		}
+	}
+</script>
