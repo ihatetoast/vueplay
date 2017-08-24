f05c3f289d6694151db96aee410ff5fbbc84cb92
Katy <ihatetoast@gmail.com>
2017-08-23 22:34:56 -0500

    .

---
+++ round2/section09/src/App.vue
@@ -0,0 +1,33 @@
+<template>
+    <div class="container">
+        <div class="row">
+            <div class="col-xs-12">
+               <app-quote>
+               		<h2 slot="h2Title">{{ quoteTitle }}</h2>
+               		<p slot="pContent">"Katy is the greatest!"</p>
+               </app-quote>
+            </div>
+        </div>
+    </div>
+</template>
+
+<script>
+/* anything else affecting the elem above (outside of style) is taken care of here
+stay here to control the vuejs syntax for what is above*/
+	import Quote from './components/Quote.vue'
+    export default {
+    	data: function(){
+    		return {
+    			quoteTitle: "The Best Quotation ever:"
+    		}
+    	},
+        components: {
+        	appQuote: Quote
+        }
+    }
+</script>
+
+<style>
+/*the style in here will not affect the html elem above. 
+to style the h2 and hte p, go to the child. */
+</style>
---
+++ round2/section09/src/components/Quote.vue
@@ -0,0 +1,29 @@
+<template>
+	<div>
+		<div>
+			<slot name="h2Title"></slot>
+		</div>
+		<hr>
+		<div>
+			<slot name="pContent"></slot>
+		</div>
+	</div>
+</template>
+
+<script>
+
+</script>
+
+<!-- the style in this child will be passed to the elements passed in from above. 
+what is below affects what goes in the slot -->
+<style scoped>
+	div {
+		border: 1px solid #ccc;
+		box-shadow: 1px 1px 2px #010101;
+		padding: 30px;
+		margin: 30px auto;
+		text-align: center;
+	}
+</style>
+
+<!-- slots go in child components -->
