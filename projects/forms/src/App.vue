446d5f22f1aa3d9362d70b3614e3469255c8b999
Katy <ihatetoast@gmail.com>
2017-09-07 23:24:00 -0500

    .

--- projects/forms/src/App.vue
+++ projects/forms/src/App.vue
@@ -1,17 +1,16 @@
 <template>
     <div class="container">
-        <form>
+        <form v-if="!submitted">
             <div class="row">
                 <div class="col-xs-12 col-sm-8 col-sm-offset-2 col-md-6 col-md-offset-3">
                     <h1>Give me all your details, suckers!</h1>
                     <!-- Exercise 1 -->
                     <!-- Create a Signup Form where you retrieve the following Information -->
                     <!-- Full Name (First Name + Last Name) -->
-                    <div class="form-group">
+                    <!-- <div class="form-group">
                         <label for="firstName">First Name</label>
                         <input
                             type="text"
-                            id="firstName"
                             class="form-control"
                             v-model = "suckerData.firstName">
                     </div>
@@ -19,48 +18,54 @@
                         <label for="lastName">Last Name</label>
                         <input
                             type="text"
-                            id="lastName"
                             class="form-control"
                             v-model = "suckerData.lastName">
-                    </div>
+                    </div> -->
+<!-- CUSTOM COMP -->
+                    <app-fullname v-model="suckerData.fullName"></app-fullname>
                     <!-- Mail -->
                     <div class="form-group">
                         <label for="email">email</label>
                         <input
                             type="text"
-                            id="email"
                             class="form-control"
                             v-model = "suckerData.email">
                     </div>
+<!-- END CUSTOM COMP -->
                     <!-- Password -->
                     <div class="form-group">
                         <label for="password">Password</label>
                         <input
                             type="password"
-                            id="password"
                             class="form-control"
                             v-model = "suckerData.password">
                     </div>
                     <!-- Store Data? Yes/No -->
-                    
-                </div>
-            </div>
-            <div class="row">
-                <div class="col-xs-12 col-sm-8 col-sm-offset-2 col-md-6 col-md-offset-3">
-                    <label for="yes">
+                    <div class="form-group">
+                        <p>Would you like to store your data for future use?</p>
+                        <label for="yes">
                         <input
                             type="radio"
-                            id="yes"
                             value="Yes"
                             v-model="storeData">Yes
-                    </label>
-                    <label for="no">
+                        </label>
+                        <label for="no">
                         <input
                             type="radio"
-                            id="no"
                             value="No"
                             v-model = "storeData">No
-                    </label>
+                        </label>
+                    </div>
+                </div>
+            </div>
+            
+            <div class="row">
+                <div class="col-xs-12 col-sm-8 col-sm-offset-2 col-md-6 col-md-offset-3">
+                    <button 
+                        class="btn btn-primary"
+                        type="submit"
+                        @click.prevent="submitData">Submit
+                    </button>
                 </div>
             </div>
         </form>
@@ -72,14 +77,16 @@
                     <!-- Exercise 3 -->
                     <!-- Edit the Example from above and create a custom "Full Name" Control -->
                     <!-- which still holds the First Name and Last Name Input Field -->
-        <div class="row">
+        <div class="row" v-if="submitted">
             <div class="col-xs-12 col-sm-8 col-sm-offset-2 col-md-6 col-md-offset-3">
                 <div class="panel panel-default">
                     <div class="panel-heading">
                         <h4>Your Data</h4>
                     </div>
                     <div class="panel-body">
-                        <p>Full Name: {{ suckerData.firstName }} {{ suckerData.lastName }}</p>
+                        <!-- <p>Full Name: {{ suckerData.firstName }} {{ suckerData.lastName }}</p> -->
+                       <!--  from custom comp -->
+                        <p>Full Name: {{ suckerData.fullName }}</p>
                         <p>Mail: {{ suckerData.email }}</p>
                         <p>Password: {{ suckerData.password }}</p>
                         <p>Store in Database?: {{ storeData }}</p>
@@ -91,17 +98,27 @@
 </template>
 
 <script>
+    import Fullname from './Fullname.vue';
     export default {
         data () {
             return{
                 suckerData: {
-                    firstName: '',
-                    lastName: '',
+                    
+                    fullName:'First Last',
                     email: '', 
-                    password: '', 
+                    password: ''
                 },
-                storeData: 'No'
+                storeData: 'No',
+                submitted: false
             }
+        },
+        methods: {
+            submitData(){
+                this.submitted = true;
+            }
+        },
+        components: {
+            appFullname: Fullname
         }
     }
 </script>
---
+++ projects/forms/src/Fullname.vue
@@ -0,0 +1,55 @@
+<template>
+	<div>
+		<div class="form-group">
+	    <label for="firstName">First Name</label>
+	    <input
+	        type="text"
+	        class="form-control"
+	        @input="nameChanged(true, $event)"
+	        :value="firstName">
+		</div>
+		<div class="form-group">
+	    <label for="lastName">Last Name</label>
+	    <input
+	        type="text"
+	        class="form-control"
+	        @input="nameChanged(false, $event)"
+	        :value="lastName">
+		</div>
+	</div>
+
+</template>
+
+<script>
+	export default {
+		props: ['value'],
+		methods: {
+			nameChanged(isFirst,e){
+				let name = '';
+				if(isFirst){
+					name = e.target.value + " " + this.lastName;
+				}
+				else{
+					name = this.firstName+ " " +e.target.value;
+				}
+				this.value = name;
+				this.$emit('input', this.value)
+			}
+		},
+		computed: {
+			firstName(){
+				return this.value.split(' ')[0];
+			},
+			lastName(){
+				return this.value.split(' ')[1];
+			}
+		}
+	}
+</script>
+
+<style scoped>
+</style>
+
+ <!-- Exercise 3 -->
+                    <!-- Edit the Example from above and create a custom "Full Name" Control -->
+                    <!-- which still holds the First Name and Last Name Input Field -->
